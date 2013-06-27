3c5fd632f3c15ae91200190b4be3202054d243f8
Andreas Marschke <andreas.marschke@gmail.com>
2013-06-27 02:08:41 +0200

    rc/emacs-rc-python.el: add highlighting for django templates

---
+++ rc/emacs-rc-python.el
@@ -0,0 +1,28 @@
+;;; emacs-rc-python.el ---
+
+;; Copyright (C) Andreas Marschke
+;;
+;; Author: andreas.marschke@gmail.com
+;; Keywords:
+;; Requirements:
+;; Status: not intended to be distributed yet
+
+;; support for Python and Django
+
+
+;; Templating helper for Django Templates
+(autoload 'django-html-mumamo-mode "~/.elisp/nxhtml/autostart.el")
+(setq auto-mode-alist
+      (append '(("\\.html?$" . django-html-mumamo-mode)) auto-mode-alist))
+(setq mumamo-background-colors nil)
+(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mumamo-mode))
+(setq mumamo-display-error-lwarn nil)
+(setq mumamo-per-buffer-local-vars-done-by-me nil)
+;; Workaround the annoying warnings:
+;;    Warning (mumamo-per-buffer-local-vars):
+;;    Already 'permanent-local t: buffer-file-name
+(when (and (equal emacs-major-version 24)
+           (>= emacs-minor-version 2))
+  (eval-after-load "mumamo"
+    '(setq mumamo-per-buffer-local-vars
+           (delq 'buffer-file-name mumamo-per-buffer-local-vars))))
