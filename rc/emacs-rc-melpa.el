78606a4ee97db32be63c7f7b16f1bae67a68233b
Andreas Marschke <andreas.marschke@gmail.com>
2017-04-21 08:03:33 +0200

    Remove emacs v24 line

--- rc/emacs-rc-melpa.el
+++ rc/emacs-rc-melpa.el
@@ -1,7 +1,5 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(when (<= emacs-major-version 24)
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
 (package-initialize)
