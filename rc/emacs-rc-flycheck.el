5490aaaf2988741546b45001183f7e5029b489fd
Andreas Marschke <andreas.marschke@gmail.com>
2017-04-21 17:54:33 +0200

    Add flycheck and projectile config

---
+++ rc/emacs-rc-flycheck.el
@@ -0,0 +1,131 @@
+;;; emacs-rc-flycheck.el --- Flycheck configuration
+
+;; Copyright (C) 2017 Andreas Marschke.
+
+;; Author: Andreas Marschke
+;; Created: 14 Jul 2010
+;; Version: 0.0
+;; Package-Requires: (())
+;; Keywords: flycheck
+;; URL:
+;;; Commentary:
+;;
+;;
+;;; Code:
+
+(require 'flycheck)
+
+(setq flycheck-checkers
+      (quote
+       (asciidoctor
+	asciidoc
+	c/c++-clang
+	c/c++-gcc
+	c/c++-cppcheck
+	css-csslint
+	css-stylelint
+	dockerfile-hadolint
+	emacs-lisp
+	emacs-lisp-checkdoc
+	groovy
+	html-tidy
+	javascript-eslint
+	javascript-standard
+	json-jsonlint
+	less
+	less-stylelint
+	lua-luacheck
+	lua
+	perl
+	perl-perlcritic
+	pug
+	python-flake8
+	python-pylint
+	python-pycompile
+	rpm-rpmlint
+	markdown-mdl
+	nix
+	scss-lint
+	scss-stylelint
+	sass/scss-sass-lint
+	sass
+	scss
+	sh-bash
+	sh-posix-dash
+	sh-posix-bash
+	sh-shellcheck
+	sql-sqlint
+	systemd-analyze
+	tex-chktex
+	tex-lacheck
+	texinfo
+	xml-xmlstarlet
+	xml-xmllint
+	yaml-jsyaml
+	yaml-ruby
+	)
+       )
+      )
+
+(defun projectile-find-flycheck-eslint-project-path ()
+  "."
+  (let ((root (ignore-errors (projectile-project-root))))
+    (when root
+      (setq my-flycheck-javascript-eslint-executable-path (concat root "node_modules/.bin/eslint"))
+      root
+      )
+    )
+  )
+
+(defvar my-flycheck-javascript-eslint-executable-path "")
+
+(flycheck-define-checker my-javascript-eslint
+  "A Javascript syntax and style checker using eslint.
+
+See URL `https://github.com/eslint/eslint'."
+  :command ("" (eval my-flycheck-javascript-eslint-executable-path) "--format=checkstyle"
+            (option-list "--rulesdir" flycheck-eslint-rules-directories)
+            "--stdin" "--stdin-filename" source-original)
+  :standard-input t
+  :error-parser flycheck-parse-checkstyle
+  :error-filter
+  (lambda (errors)
+    (seq-do (lambda (err)
+              ;; Parse error ID from the error message
+              (setf (flycheck-error-message err)
+                    (replace-regexp-in-string
+                     (rx " ("
+                         (group (one-or-more (not (any ")"))))
+                         ")" string-end)
+                     (lambda (s)
+                       (setf (flycheck-error-id err)
+                             (match-string 1 s))
+                       "")
+                     (flycheck-error-message err))))
+            (flycheck-sanitize-errors errors))
+    errors)
+  :enabled (lambda () (flycheck-eslint-config-exists-p))
+  :modes (js-mode js-jsx-mode js2-mode js2-jsx-mode js3-mode rjsx-mode)
+  :next-checkers ((warning . javascript-jscs))
+  :verify
+  (lambda (_)
+    (let* ((default-directory
+             (projectile-find-flycheck-eslint-project-path))
+           (have-config (flycheck-eslint-config-exists-p)))
+      (list
+       (flycheck-verification-result-new
+        :label "config file"
+        :message (if have-config "found" "missing")
+        :face (if have-config 'success '(bold error)))))))
+
+
+(eval-after-load 'flycheck
+  '(progn
+     (flycheck-add-mode 'html-tidy 'web-mode)
+     (flycheck-add-mode 'css-csslint 'web-mode)
+     (flycheck-add-mode 'javascript-eslint 'web-mode)))
+
+;;;;
+
+
+
--- rc/emacs-rc-projectile.el
+++ rc/emacs-rc-projectile.el
@@ -1,14 +1,32 @@
-(add-to-list 'js2-mode-hook 'projectile-mode)
-(add-to-list 'java-mode-hook 'projectile-mode)
-(add-to-list 'js-mode-hook 'projectile-mode)
+;;; emacs-rc-projectile.el --- Projectile configuration
 
-(setq flx-ido-mode t)
+;; Copyright (C) 0000 Free Software Foundation, Inc.
 
-(setq projectile-completion-system 'ido)
+;; Author:  <@>
+;; Created: 14 Jul 2010
+;; Version: 0.0
+;; Package-Requires: (())
+;; Keywords: projectile
+;; URL:
+;;; Commentary:
+;; 
+;;
+;;; Code:
+
+;;(add-to-list 'js2-mode-hook 'projectile-mode)
+;;(add-to-list 'java-mode-hook 'projectile-mode)
+;;(add-to-list 'js-mode-hook 'projectile-mode)
+
+(defvar flx-ido-mode t)
+(defvar projectile-completion-system 'ido)
+
+(projectile-global-mode)
 
 (defun soasta-mode ()
+  "SOASTA style indentation and coding style."
+  
   (interactive)
-  (setq current-fill-column 240)
+  (defvar current-fill-column 240)
   (setq indent-tabs-mode nil)
   (setq tab-width 2)
   (setq js2-basic-offset 2)
@@ -35,8 +53,9 @@
   )
 
 (defun soasta-js-mode ()
+  "SOASTA Javascript library configuration."
   (interactive)
-  (setq current-fill-column 240)
+  (defvar current-fill-column 240)
   (setq indent-tabs-mode nil)
   (setq tab-width 2)
 
@@ -49,33 +68,87 @@
   (setq c-basic-offset '(. 2))
 
   (setq c-offsets-alist
-	'(quote
-	  (member-init-intro     . +)
-	  (substatement-open     . +)
-	  (statement-block-intro . +)
-	  (func-decl-cont        . 0)
-	  (topmost-intro         . +)
-	  (inline-open           . 0)
-	  (block-open            . +)
-	  ))
+    '(quote
+      (member-init-intro     . +)
+      (substatement-open     . +)
+      (statement-block-intro . +)
+      (func-decl-cont        . 0)
+      (topmost-intro         . +)
+      (inline-open           . 0)
+      (block-open            . +)
+      ))
   (c-toggle-auto-hungry-state 2)
   )
 
 
 (defun boomerang-mode ()
+  "Boomerang.js indentation and coding style definition."
   (interactive)
-  (setq current-fill-column 240)
+  (defvar current-fill-column 240)
   (setq indent-tabs-mode t)
   (setq tab-width 4)
-  (setq hs-minor-mode t)
-  (setq auto-fill-mode nil)
-  (setq js2-basic-offset 4)
-  (setq js2-bounce-indent-p t)
-  (setq c-basic-offset '(. 4))
-  (setq c-set-offset '(member-init-intro '++))
+  (defvar hs-minor-mode t)
+  (defvar auto-fill-mode nil)
+  (defvar js2-basic-offset 4)
+  (defvar js2-bounce-indent-p t)
+  (defvar c-basic-offset '(. 4))
+  (defvar c-set-offset '(member-init-intro '++))
   (c-set-offset 'substatement-open 0)
   (c-set-offset 'block-open '0)
   (c-toggle-auto-hungry-state 0)
   )
 
+(projectile-register-project-type 'boomerang '("boomerang.js")
+				  :compile "grunt --no-color lint"
+				  :test "grunt --no-color lint"
+				  :run "grunt --no-color test:debug"
+				  :test-suffix ".js")
 
+(projectile-register-project-type 'boomerang-android '("mpulse-android")
+				  :compile "gradle clean build -Pbuildnumber=1-x"
+				  :test "gradle clean build test -Pbuildnumber=1-x"
+				  :run "gradle clean build test -Pbuildnumber=1-x"
+				  :test-suffix ".java")
+
+(projectile-register-project-type 'mpulse-java '("test-app")
+				  :compile "gradle clean build -Pbuildnumber=1-x"
+				  :test "gradle clean build test -Pbuildnumber=1-x"
+				  :run "gradle clean build test -Pbuildnumber=1-x"
+				  :test-suffix ".java")
+
+(defun flycheck-checker-javascript-eslint-find-binary-hook ()
+  "Simple method for setting correct eslint version."
+  (if (projectile-project-p)
+      (message "Projectile switched to project checking for correct place of linter...")
+    (when (eq 'boomerang (projectile-project-type))
+      (message (concat "Found Boomerang project setting eslint executable to: " (concat (projectile-project-root) "node_modules/.bin/eslint")))
+      (defvar flycheck-javascript-eslint-executable (concat (projectile-project-root) "node_modules/.bin/eslint"))
+      (boomerang-mode)
+      )
+    )
+  )
+
+(defun cc-mode-indentation-mode-hook()
+  (if (projectile-project-p)
+      (when (eq 'boomerang (projectile-project-type))
+	(when (string-suffix-p ".js" (buffer-file-name (current-buffer)))
+	  (or (string-suffix-p ".html" (buffer-file-name (current-buffer)))
+	      (boomerang-mode))
+	  (or (string-suffix-p ".jsx" (buffer-file-name (current-buffer)))
+	      (boomerang-mode))
+	  (boomerang-mode))))
+  )
+
+
+(add-hook 'projectile-after-switch-project-hook 'flycheck-checker-javascript-eslint-find-binary-hook)
+(add-hook 'projectile-find-file-hook 'flycheck-checker-javascript-eslint-find-binary-hook)
+(add-hook 'projectile-find-dir-hook 'flycheck-checker-javascript-eslint-find-binary-hook)
+(add-hook 'find-file-hook 'flycheck-checker-javascript-eslint-find-binary-hook)
+
+(add-hook 'projectile-find-file-hook 'cc-mode-indentation-mode-hook)
+(add-hook 'projectile-find-dir-hook 'cc-mode-indentation-mode-hook)
+(add-hook 'find-file-hook 'cc-mode-indentation-mode-hook)
+
+;;(provide emacs-rc-projectile)
+
+;;; emacs-rc-projectile.el ends here
