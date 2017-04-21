cc2b94700c9103f8a007083bc22363fafd48fd63
Andreas Marschke <andreas.marschke@gmail.com>
2017-04-21 07:42:25 +0200

    Add Checkstyle compiler line parsing and javadoc lookup

--- rc/emacs-rc-java.el
+++ rc/emacs-rc-java.el
@@ -8,3 +8,12 @@
 (add-to-list 'compilation-error-regexp-alist '("^:\w+:[a-zA-Z]*\\([\w/]\\):\\([0-9]*\\):\\(\w+\\)" 1 2 4))
 
 (add-to-list 'compilation-error-regexp-alist '("^\\(.*\\)\\.java: line \\([0-9]*\\), col \\([0-9]*\\), \\(Error\\|Warning\\) - \\(.*\\)$" 1 2 3))
+
+;; Checkstyle matching from gradle
+(add-to-list 'compilation-error-regexp-alist '("\\[ant:checkstyle] \\[\\(WARN\\|ERROR\\)] \\(.*\\):\\([0-9]*\\):\\([0-9]*\\): .*" 2 3))
+(add-to-list 'compilation-error-regexp-alist '("\\[ant:checkstyle] \\[\\(WARN\\|ERROR\\)] \\(.*\.java\\):\\([0-9]*\\): .*" 2 3))
+
+(javadoc-add-artifacts [org.json json "20160212"]
+                       [org.apache.httpcomponents httpclient "4.3.3"]
+                       [junit junit "4.12"]
+		       [com.android.tools.build gradle "2.3.0"])
