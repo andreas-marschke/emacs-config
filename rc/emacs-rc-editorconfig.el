59ebbd1c0a8050d0b42fd5847391bf78c158942e
Andreas Marschke <andreas.marschke@gmail.com>
2018-01-01 13:39:22 +0100

    Added new dependencies

---
+++ rc/emacs-rc-editorconfig.el
@@ -0,0 +1,17 @@
+;;; emacs-rc-editorconfig.el --- EditorConfig.org configuration for Emacs
+
+;; Copyright (C) 2017 Andreas Marschke
+
+;; Author:  <emacs@andreas-marschke.name>
+;; Created: 17.05.2017
+;; Version: 0.
+;; Package-Requires: (())
+;; Keywords: 
+;; URL:
+;;; Commentary:
+;; 
+;;
+;;; Code:
+
+(custom-set-variables
+ '(editorconfig-mode 1))
